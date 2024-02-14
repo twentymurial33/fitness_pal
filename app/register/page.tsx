a37c5cd63254da271950a6372ba2340abfa2fc6d
Murial Anindo <murial.anindo@lonett.com>
2024-02-14 16:40:55 -0500

    connected with vercel postgres and next auth

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+export default async function RegisterPage(){
+
+    return(
+        <form>
+            <input type="email"/>
+            <input type="password"/>
+            <button type="submit">Register</button>
+        </form>
+    )
+}
