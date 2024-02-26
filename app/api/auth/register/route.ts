1a4c9b7eddf5d325dda69a8535b61f3f4c16395d
Murial Anindo <murial.anindo@lonett.com>
2024-02-25 20:27:46 -0500

    added validation for email and password

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,10 +1,15 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
+import {hash} from 'bcrypt';
+import {sql} from "@vercel/postgres";
 
 export default async function POST(request:Request){
     try{
      const {email, password }= await request.json()
-     //validate email and password 
      console.log({email,password})
+     const hashedPassword=await hash(password,10);
+     const response=await sql`
+     INSERT INTO users(email,password)
+     `
     }catch(e){
   console.log({e})
     }
