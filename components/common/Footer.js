ff1b9f55bf55c6deee76aa96e89a2479b925fc4d
Murial Anindo <murial.anindo@lonett.com>
2024-04-11 18:25:52 -0400

    updated footer and npm install

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -18,35 +18,13 @@ export default function StickyFooter() {
     <ThemeProvider theme={defaultTheme}>
       <FooterContainer>
         <FooterContent>
-          <FooterSection>
-            <FooterTitle>About Us</FooterTitle>
-            <FooterText>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </FooterText>
-          </FooterSection>
-          <FooterSection>
-            <FooterTitle>Contact Us</FooterTitle>
+          <FooterSection></FooterSection>
+          <FooterSection style={{ marginRight: "380px" }}>
             <FooterText>Email: example@example.com</FooterText>
             <FooterText>Phone: 123-456-7890</FooterText>
           </FooterSection>
-          <FooterSection>
-            {/* <FooterTitle>Follow Us</FooterTitle> */}
-            {/* <SocialIcons>
-              <a href="#">
-                <i className="fab fa-facebook"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram"></i>
-              </a>
-            </SocialIcons> */}
-          </FooterSection>
+          <FooterSection></FooterSection>
         </FooterContent>
-        <div className="footer__bottom">
-          <p>&copy; 2024 Your Company. All rights reserved.</p>
-        </div>
       </FooterContainer>
     </ThemeProvider>
   );
